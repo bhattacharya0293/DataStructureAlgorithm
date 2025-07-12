5569c0cb9ed91eaec48e2ac668abeceb7506dd2b
bhattacharya0293 <bhattacharya0293@gmail.com>
2025-07-12 03:17:50 -0500

    3rd commit backtracking

--- src/main/java/Backtracking/NQueensCountWays.java
+++ src/main/java/Backtracking/NQueensCountWays.java
@@ -1,6 +1,6 @@
 package Backtracking;
 
-public class NQueensAllways {
+public class NQueensCountWays {
     public static boolean isSafe(char[][] board, int row, int col){
         //vertical up
         for(int i=row-1; i>=0; i--){
@@ -22,38 +22,31 @@ public class NQueensAllways {
         }
         return true;
     }
-    public static void nQueen(char[][] board, int row){
+    public static void nQueens(char[][] board, int row){
         //base
         if(row == board.length){
-            printBoard(board);
+            count++;
             return;
         }
-        //cols
+        //recursion
         for(int j=0; j<board.length; j++){
             if(isSafe(board, row, j)){
                 board[row][j] = 'Q';
-                nQueen(board, row+1);
+                nQueens(board, row+1);
                 board[row][j] = 'x';
             }
         }
     }
-    public static void printBoard(char[][] board){
-        System.out.println("------ Chess board ------");
-        for(int i=0; i<board.length; i++){
-            for(int j=0; j<board.length; j++){
-                System.out.print(board[i][j]+ " ");
-            }
-            System.out.println("");
-        }
-    }
+    static int count = 0;
     public static void main(String[] args){
         int n = 5;
         char[][] board = new char[n][n];
         for(int i=0; i<board.length; i++){
-            for (int j=0; j<board.length; j++){
+            for(int j=0; j<board.length; j++){
                 board[i][j] = '.';
             }
         }
-        nQueen(board, 0);
+        nQueens(board, 0);
+        System.out.print("Number of way for n Queens to be safe: "+count);
     }
 }
--- src/main/java/Backtracking/NQueensPrintOneSolution.java
+++ src/main/java/Backtracking/NQueensPrintOneSolution.java
@@ -1,6 +1,6 @@
 package Backtracking;
 
-public class NQueensAllways {
+public class NQueensPrintOneSolution {
     public static boolean isSafe(char[][] board, int row, int col){
         //vertical up
         for(int i=row-1; i>=0; i--){
@@ -22,38 +22,45 @@ public class NQueensAllways {
         }
         return true;
     }
-    public static void nQueen(char[][] board, int row){
+    public static boolean nQueens(char[][] board, int row){
         //base
         if(row == board.length){
-            printBoard(board);
-            return;
+            return true;
         }
-        //cols
+        //col
         for(int j=0; j<board.length; j++){
             if(isSafe(board, row, j)){
                 board[row][j] = 'Q';
-                nQueen(board, row+1);
+                if(nQueens(board, row+1)){
+                    return true;
+                }
                 board[row][j] = 'x';
             }
         }
+        return false;
     }
     public static void printBoard(char[][] board){
-        System.out.println("------ Chess board ------");
+        System.out.println("----- Chess Board ------");
         for(int i=0; i<board.length; i++){
-            for(int j=0; j<board.length; j++){
+            for (int j=0; j<board.length; j++){
                 System.out.print(board[i][j]+ " ");
             }
-            System.out.println("");
+            System.out.println(" ");
         }
     }
     public static void main(String[] args){
-        int n = 5;
+        int n=5;
         char[][] board = new char[n][n];
         for(int i=0; i<board.length; i++){
-            for (int j=0; j<board.length; j++){
+            for(int j=0; j<board.length; j++){
                 board[i][j] = '.';
             }
         }
-        nQueen(board, 0);
+        if(nQueens(board, 0)){
+            System.out.println("Solution is possible");
+           printBoard(board);
+        }else{
+            System.out.println("Solution is not possible");
+        }
     }
 }
